6c349460296a5378fa6c915abcd7b4660b5dcdb6
Adam Onishi <onishiweb@gmail.com>
2026-01-04 12:59:25 +0000

    Add day 4 prompt

--- app/utils/prompts.ts
+++ app/utils/prompts.ts
@@ -2,4 +2,5 @@ export const prompts = [
     "Write about someone celebrating something new in their life.",
     "Write about a hard choice someone has to make about leaving",
     "Write something using the theme 'completion'",
+    "Write about someone not wanting to do something the following day",
 ];
