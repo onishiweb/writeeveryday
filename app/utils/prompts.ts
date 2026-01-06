2ce5f0269a0c671304a0dd6270dd64ea88364d06
Adam Onishi <onishiweb@gmail.com>
2026-01-06 20:01:55 +0000

    Add prompts up to Jan 10

--- app/utils/prompts.ts
+++ app/utils/prompts.ts
@@ -1,4 +1,5 @@
 export const prompts = [
+    // January
     "Write about someone celebrating something new in their life.",
     "Write about a hard choice someone has to make about leaving",
     "Write something using the theme 'completion'",
@@ -6,4 +7,8 @@ export const prompts = [
     "Write using the theme 'laziness'",
     "Write a story centered around an unwanted gift",
     "Write using the theme 'loneliness'",
+    "Write a scene from a fantasy novel where a character is discovering a new power",
+    "Write using the theme 'motivation'",
+    "Write a scene from a crime novel where a crime has been discovered",
+    // January 11
 ];
