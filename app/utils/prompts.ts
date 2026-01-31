d645c118e9440188b5b552b71c4935bdd05c0ebc
Adam Onishi <onishiweb@gmail.com>
2026-01-31 19:50:56 +0000

    Add early Feb prompts

--- app/utils/prompts.ts
+++ app/utils/prompts.ts
@@ -9,31 +9,42 @@ export const prompts = [
     "Write using the theme 'loneliness'",
     "Write a scene from a fantasy novel where a character is discovering a new power",
     "Write using the theme 'motivation'",
-    "Write a scene from a crime novel where a crime has been discovered",
-    // January 11
-    "Write about someone who is nervous about trying something new", // 11
-    "Write using the theme 'anxiety'", // 12
-    "Write a positive review of a new kitchen utensil", // 13
-    "Write a world building scene for a fantasy world from a first person viewpoint", // 14
+    "Write a scene from a crime novel where a crime has been discovered", 
+    
+    "Write about someone who is nervous about trying something new", // January 11
+    "Write using the theme 'anxiety'",
+    "Write a positive review of a new kitchen utensil",
+    "Write a world building scene for a fantasy world from a first person viewpoint",
     "Write a negative review of a fictional restaurant", // 15
-    "Write a scene with a character deciding their first wish from a genie", // 16
-    "Write using the theme 'warmth'", // 17
-    "Write an argument between two friends about what to do that evening", // 18
-    "Write using the theme 'blue'", // 19
+    "Write a scene with a character deciding their first wish from a genie",
+    "Write using the theme 'warmth'",
+    "Write an argument between two friends about what to do that evening",
+    "Write using the theme 'blue'",
     "Write about your first time", // 20 
-    // January 21
-    "Write a story about someone that knows something they shouldn't",
+    
+    "Write a story about someone that knows something they shouldn't", // January 21
     "Write about someone preparing for an interview",
     "Write using the theme 'adventure'",
     "Write a scene from a romance where a character notices their love interest for the first time",
-    "Write something loud",
+    "Write something loud", // 25
     "Write a day in the life of a cat",
     "Write about a journey someone is about to embark on",
     "Write using the theme 'chaos'",
     "Write about something you have a strong opinion about",
-    "Write a scene from a horror where the characters enter a haunted house",
-    // January 31
-    "Write about the feeling of finally making it through",
+    "Write a scene from a horror where the characters enter a haunted house", // 30
+    
+    "Write about the feeling of finally making it through", // January 31
 
     // February
+    "Write a story that has to do with a particular time period",
+    "Write a recipe",
+    "Write a day in the life of a dog",
+    "Write using the theme 'companionship'",
+    "Write a fight scene", // 5
+    "Write a scene that's set on a spaceship",
+    "Write about grief",
+    "Write your thoughts about social media",
+    "Write using the theme 'exploration'",
+    "Write a dialogue between two characters in the style of a screenplay", // 10
+
 ];
